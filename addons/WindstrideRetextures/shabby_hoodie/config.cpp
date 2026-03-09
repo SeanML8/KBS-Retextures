0a242a86c8290c668f7dbcb1c45267f2e7318d13
sean <sean.anthony.beck@gmail.com>
2026-03-09 17:21:31 +0000

    added test shabby hoodie

--- addons/WindstrideRetextures/shabby_hoodie/config.cpp
+++ addons/WindstrideRetextures/shabby_hoodie/config.cpp
@@ -31,4 +31,13 @@ class CfgVehicles
 		hiddenSelectionsTextures[] = {"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\BUN_shabby_hoodie_thrifted_color_block.paa",
 		"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\skein_grey_ca.paa"};
 	};
+	class Shabby_Hoodie_ColorBase;
+	class Shabby_Hoodie_Thrifted_Color_Block_Test: Shabby_Hoodie_ColorBase
+	{
+		scope = 2;
+		displayName = "Thrifted Color Block Shabby Hoodie 2";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] = {"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\psd.paa",
+		"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\skein_grey_ca.paa"};
+	};
 };
