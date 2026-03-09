5b7857ce05b7cb2ba76b50a0d747ec2d5b8b6c7b
sean <sean.anthony.beck@gmail.com>
2026-03-09 08:00:36 +0000

    added short jeans added new shabby hoodie

---
+++ addons/DZBase/shorts_jean/config.cpp
@@ -0,0 +1,156 @@
+class CfgPatches
+{
+	class KTT_ShortsJean
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {
+			"DZ_Data", "DZ_Characters"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_ArmyGreen: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Army Green Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_armygreen.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_armygreen.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_armygreen.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_armygreen.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_CamelBrown: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Camel Brown Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_camelbrown.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_camelbrown.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_camelbrown.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_camelbrown.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_DustyRose: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Dusty Rose Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_dustyrose.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_dustyrose.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_dustyrose.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_dustyrose.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Eggplant: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Eggplant Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_eggplant.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_eggplant.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_eggplant.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_eggplant.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Emerald: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Emerald Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_emerald.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_emerald.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_emerald.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_emerald.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Obsidian: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Obsidian Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_obsidian.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_obsidian.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_obsidian.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_obsidian.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_RedWine: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Red Wine Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_redwine.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_redwine.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_redwine.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_redwine.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Sapphire: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Sapphire Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_sapphire.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_sapphire.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_sapphire.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_sapphire.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Slate: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Slate Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_slate.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_slate.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_slate.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_slate.paa"
+		};
+	};
+	class ShortJean_ColorBase;
+	class KTT_Shorts_Jean_Teal: ShortJean_ColorBase
+	{
+		scope = 2;
+		displayName = "Teal Jean Shorts";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] =
+		{
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_teal.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_teal.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_teal.paa",
+			"\KBS-Retextures\addons\DZBase\shorts_jean\KTT_shortjeans_teal.paa"
+		};
+	};
+};
--- addons/WindstrideRetextures/shabby_hoodie/config.cpp
+++ addons/WindstrideRetextures/shabby_hoodie/config.cpp
@@ -22,4 +22,13 @@ class CfgVehicles
 		hiddenSelectionsTextures[] = {"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\BUN_Shabby_Hoodie_Thrifted_Floral.paa",
 		"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\skein_grey_ca.paa"};
 	};
+	class Shabby_Hoodie_ColorBase;
+	class Shabby_Hoodie_Thrifted_CB2: Shabby_Hoodie_ColorBase
+	{
+		scope = 2;
+		displayName = "Thrifted CB Shabby Hoodie";
+		descriptionShort = "Found in a Shipping Container. Designed and Made by Canadian and Norweigan children.";
+		hiddenSelectionsTextures[] = {"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\BUN_shabby_hoodie_thrifted_cb2.paa",
+		"\KBS-Retextures\addons\WindstrideRetextures\shabby_hoodie\skein_grey_ca.paa"};
+	};
 };
